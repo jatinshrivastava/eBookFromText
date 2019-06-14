adff04e85e3ac296a30baba0da9522d8a2bab77a
Jatin Shrivastava <31873847+jatinsrivastava@users.noreply.github.com>
2019-06-14 12:48:00 +0530

    Delete PatternMatching.py

--- PatternMatching.py
+++
@@ -1,4 +0,0 @@
-import re
-text = "﻿[14/08/18, 7:50:51 PM] Aashish: Good intentions without actions, is like Soul without body, However pure it is, it can not bring any difference"  
-result = re.sub(r'(([ [ ]\d\d/\d\d/\d\d, [0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])(:([0-5]?[0-9] [AP]M[]] [a-zA-Z]*[:]))?', "", text)  
-print(result)  
