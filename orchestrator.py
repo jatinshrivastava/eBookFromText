8a976d426aab8ad0420fa6be51fd4d2a3e9c7eca
Jatin Shrivastava <31873847+jatinsrivastava@users.noreply.github.com>
2019-06-19 22:30:38 +0530

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -5,8 +5,11 @@ from chatParser import WhatsAppChatParser
 from slideGenerator import SlideGenerator 
 
 def main():
+    if ( len(sys.argv) < 3 ):
+        print("Please specify the exportfile and desired output filename\n Usage: " + sys.argv[0] + " <chatexportFile> <pptfilename> " )
+        return
     chatparser = WhatsAppChatParser(sys.argv[1])
-    generator = SlideGenerator('output.pptx')
+    generator = SlideGenerator(sys.argv[2])
     
     while True:
         try:
