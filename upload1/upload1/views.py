36e84261acc56b9772fada55b8bd5e808ce009f1
Sumit Raj <42216434+sumit998@users.noreply.github.com>
2019-06-24 20:20:34 +0530

    Delete views.py

--- upload1/upload1/views.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.conf import settings
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from uploadmethod.forms import uploadFileForm
-from django.core.files.storage import FileSystemStorage
